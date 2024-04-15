1ee01aba8519bdcc260339ad259022230ecbc7c5
crabcreator <playnik3@gmail.com>
2024-04-15 13:41:44 +0300

    Upd: Header

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect, useRef, useContext } from "react";
-import { CityContext } from "../../Contexts/CityContext";
-import { LangContext, langs } from "../../Contexts/LangContext";
-import { Link } from "react-router-dom";
-import axios from "axios";
-import "./Header.css";
+import React, { useState, useEffect, useRef, useContext } from 'react';
+import { CityContext } from '../../Contexts/CityContext';
+import { LangContext, langs } from '../../Contexts/LangContext';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import './Header.css';
 
 export default function Header() {
   const {
@@ -15,8 +15,15 @@ export default function Header() {
 
   const [searchIsOpen, setSearchOpen] = useState(false);
   const [langIsOpen, setLangOpen] = useState(false);
-  const [searchValue, setSearchValue] = useState("");
-  const [searchResults, setSearchResults] = useState({ name: "", id: 0 });
+  const [searchValue, setSearchValue] = useState('');
+  const [searchResults, setSearchResults] = useState({ name: '', id: 0 });
+
+  const jsonString = window.localStorage.getItem('LastCities');
+  let lastCitiesArray: LastCity[] = [{ name: '', id: undefined }];
+  if (jsonString) {
+    lastCitiesArray = JSON.parse(jsonString);
+  }
+  const [lastCities, setLastCities] = useState(lastCitiesArray);
 
   let menuRef = useRef<HTMLDivElement>(null);
   let langRef = useRef<HTMLDivElement>(null);
@@ -28,6 +35,27 @@ export default function Header() {
     setSearchValue(e.target.value);
   };
 
+  type LastCity = {
+    name: string;
+    id: number | undefined;
+  };
+
+  const updateLastCities = (newCity: LastCity) => {
+    let isInList = false;
+    for (let i = 0; i < lastCities.length; i++) {
+      if (lastCities[i].name === newCity.name) {
+        isInList = true;
+      }
+    }
+    if (!isInList) {
+      let tempArray: LastCity[] = lastCities;
+      tempArray.push({ name: newCity.name, id: newCity.id });
+      setLastCities(tempArray);
+      console.log(lastCities);
+      window.localStorage.setItem('LastCities', JSON.stringify(lastCities));
+    }
+  };
+
   useEffect(() => {
     if (searchValue) {
       axios
@@ -35,19 +63,16 @@ export default function Header() {
           `https://geocoding-api.open-meteo.com/v1/search?name=${searchValue}&count=1&language=ua&format=json`
         )
         .then((response) => {
-          if (response.data.results[0].country === "Ukraine") {
+          if (response.data.results[0].country === 'Ukraine') {
             setSearchResults({
               name: response.data.results[0].name,
               id: response.data.results[0].id,
             });
-            console.log(searchResults);
           }
         })
         .catch((error) => console.log(error));
     }
-  }, [searchValue]);
-
-  console.log(cityName);
+  }, [searchValue, searchResults]);
 
   useEffect(() => {
     let handler = (
@@ -57,8 +82,8 @@ export default function Header() {
         setSearchOpen(false);
       }
     };
-    document.addEventListener("mousedown", handler as any);
-    return () => document.removeEventListener("mousedown", handler as any);
+    document.addEventListener('mousedown', handler as any);
+    return () => document.removeEventListener('mousedown', handler as any);
   }, []);
   useEffect(() => {
     let handler = (
@@ -68,94 +93,116 @@ export default function Header() {
         setLangOpen(false);
       }
     };
-    document.addEventListener("mousedown", handler as any);
-    return () => document.removeEventListener("mousedown", handler as any);
+    document.addEventListener('mousedown', handler as any);
+    return () => document.removeEventListener('mousedown', handler as any);
   }, []);
 
   return (
-    <div className="navigation">
+    <div className='navigation'>
       <header>
-        <div className="burger">
-          <img src="./img/header/burger-icon.png" alt="Menu" />
+        <div className='burger'>
+          <img src='./img/header/burger-icon.png' alt='Menu' />
         </div>
-        <div className="logo">
-          <Link to="/">
-            <img src="./img/header/Logo.png" alt="Logo" />
+        <div className='logo'>
+          <Link to='/'>
+            <img src='./img/header/Logo.png' alt='Logo' />
           </Link>
         </div>
-        <div className="search">
+        <div className='search'>
           <div
-            className="main-search"
+            className='main-search'
             onClick={() => setSearchOpen(!searchIsOpen)}>
-            <div className="location-sign">
-              <img src="/img/header/location-sign.png" alt="" />
+            <div className='location-sign'>
+              <img src='/img/header/location-sign.png' alt='' />
             </div>
-            <span className="select-city">
-              {cityName === "" ? "Вибрати місто" : cityName}
-            </span>
-            <div className="drop-arrow">
-              <img src="/img/header/menu-drop-arrow.png" alt="" />
+            <span className='select-city'>{cityName || 'Вибрати місто'}</span>
+            <div className='drop-arrow'>
+              <img src='/img/header/menu-drop-arrow.png' alt='' />
             </div>
           </div>
           <div
-            className={`dropdown-search ${searchIsOpen ? "active" : ""}`}
+            className={`dropdown-search ${searchIsOpen ? 'active' : ''}`}
             ref={menuRef}>
-            <div className="select-city-drop">
-              <img src="/img/header/active-search.png" alt="" />
+            <div className='select-city-drop'>
+              <img src='/img/header/active-search.png' alt='' />
               <input
-                type="text"
-                placeholder="Місто, область"
+                type='text'
+                placeholder='Місто, область'
                 onChange={(e) => changeValue(e)}
                 value={searchValue}
               />
             </div>
-            <div className="search-results" ref={resultsRef}>
-              <ul className="res-cities-list">
-                <li className="res-cities-list-el">
-                  <button
-                    onClick={() => {
-                      setCity(searchResults.name, searchResults.id);
-                      setSearchValue("");
-                      setSearchOpen(false);
-                      setSearchResults({ name: "", id: 0 });
-                    }}>
-                    {searchResults.name}
-                  </button>
+            <div
+              className='search-results'
+              ref={resultsRef}
+              style={{
+                padding: `${
+                  resultsRef.current?.innerText === '' ? 0 : '16px 0 16px 10px'
+                }`,
+              }}>
+              <ul className='res-cities-list'>
+                <li className='res-cities-list-el'>
+                  {searchResults.name !== '' ? (
+                    <button
+                      onClick={() => {
+                        setCity(searchResults.name, searchResults.id);
+                        console.log(cityName);
+                        setSearchValue('');
+                        setSearchOpen(false);
+                        updateLastCities({
+                          name: searchResults.name,
+                          id: searchResults.id,
+                        });
+                        setSearchResults({ name: '', id: 0 });
+                      }}>
+                      {searchResults.name}
+                    </button>
+                  ) : (
+                    ''
+                  )}
                 </li>
               </ul>
             </div>
-            <div className="turn-on-gps">
-              <img src="/img/header/gps.png" alt="" />
+            <div className='turn-on-gps'>
+              <img src='/img/header/gps.png' alt='' />
               <span>Моє місцезнаходження</span>
             </div>
-            <div className="previous-cities">
+            <div className='previous-cities'>
               <span>Ви нещодавно дивилися</span>
-              <ul className="last-cities-list">
-                {/* {cities.map((el, index) => (
-                                    <li key={el} className="last-cities-list-el"><button onClick={() => {
-                                        setCity(index);
-                                        setSearchOpen(false);
-                                    }}>{el}</button></li>
-                                ))} */}
+              <ul className='last-cities-list'>
+                {lastCities.map((el, index) => (
+                  <li className='last-cities-list-el' key={index}>
+                    <button
+                      onClick={() => {
+                        setCity(el.name, el.id);
+                        console.log(cityName);
+                        setSearchValue('');
+                        setSearchOpen(false);
+                        updateLastCities({ name: el.name, id: el.id });
+                      }}>
+                      {el.name}
+                    </button>
+                  </li>
+                ))}
               </ul>
             </div>
           </div>
         </div>
-        <div className="lang" onClick={() => setLangOpen(!langIsOpen)}>
-          <div className="earth-sign">
-            <img src="/img/header/earth.png" alt="" />
+        <div className='lang' onClick={() => setLangOpen(!langIsOpen)}>
+          <div className='earth-sign'>
+            <img src='/img/header/earth.png' alt='' />
           </div>
-          <span className="selected-lang">{lang}</span>
-          <div className="drop-arrow">
-            <img src="/img/header/menu-drop-arrow.png" alt="" />
+          <span className='selected-lang'>{lang}</span>
+          <div className='drop-arrow'>
+            <img src='/img/header/menu-drop-arrow.png' alt='' />
           </div>
         </div>
         <div
-          className={`dropdown-lang ${langIsOpen ? "active" : ""}`}
+          className={`dropdown-lang ${langIsOpen ? 'active' : ''}`}
           ref={langRef}>
-          <ul className="lang-list">
+          <ul className='lang-list'>
             {langs.map((el, index) => (
-              <li key={el} className="lang-list-el">
+              <li key={el} className='lang-list-el'>
                 <button
                   onClick={() => {
                     setLang(index);
@@ -168,25 +215,6 @@ export default function Header() {
           </ul>
         </div>
       </header>
-      <div className="sub-nav">
-        <div className="date-range">
-          <nav className="date-tab opened">
-            <button>Сьогодні</button>
-          </nav>
-          <nav className="date-tab">
-            <button>Завтра</button>
-          </nav>
-          <nav className="date-tab">
-            <button>Тиждень</button>
-          </nav>
-          <nav className="date-tab">
-            <button>Місяць</button>
-          </nav>
-          <nav className="date-tab">
-            <button>Погодинно</button>
-          </nav>
-        </div>
-      </div>
     </div>
   );
 }
