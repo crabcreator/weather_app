f6fc66f7ac1c56eaa7ec66affad9874e652316d5
crabcreator <playnik3@gmail.com>
2024-04-15 14:21:01 +0300

    fix: Contexts

--- src/Contexts/LangContext.tsx
+++ src/Contexts/LangContext.tsx
@@ -1,26 +1,31 @@
-import React, { createContext, useContext, useState, JSX } from "react";
+import React, { createContext, useContext, useState, JSX } from 'react';
 
-export const langs = [
-   "UA", "EN", "DE", "NE", "FR"
-];
+export const langs = ['UA', 'EN', 'DE', 'NE', 'FR'];
 
 interface ILang {
-   lang: string,
-   setLang: (id:number) => void,
+  lang: string;
+  setLang: (id: number) => void;
 }
 
-export const LangContext = createContext<ILang>({lang: window.localStorage.getItem("Lang") || "UA", setLang: (id) => {},});
+export const LangContext = createContext<ILang>({
+  lang: window.localStorage.getItem('Lang') || 'UA',
+  setLang: (id) => {},
+});
 
-export function LangContextProvider(props:{children:JSX.Element}) {
-   const [currentLang, setCurrentLang] = useState<string>(useContext(LangContext).lang);
-   return (
-      <LangContext.Provider value={{lang:currentLang, setLang: function(id:number) {
-         if(id) {
-            setCurrentLang(langs[id]);
-            window.localStorage.setItem("Lang", langs[id]);
-         }
-      }}}>
-         {props.children}
-      </LangContext.Provider>
-   )
+export function LangContextProvider(props: { children: JSX.Element }) {
+  const [currentLang, setCurrentLang] = useState<string>(
+    useContext(LangContext).lang
+  );
+  return (
+    <LangContext.Provider
+      value={{
+        lang: currentLang,
+        setLang: function (id: number) {
+          setCurrentLang(langs[id]);
+          window.localStorage.setItem('Lang', langs[id]);
+        },
+      }}>
+      {props.children}
+    </LangContext.Provider>
+  );
 }
